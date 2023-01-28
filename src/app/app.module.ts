f448575a818b2470f47652e394fbe496e46a13d5
Hector Rodriguez <hector@hrod.dev>
2023-01-28 16:37:05 -0500

    First commit. Empty skelton.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { TaskComponent } from './task/task.component';
+import { TasklistComponent } from './tasklist/tasklist.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TaskComponent,
+    TasklistComponent
   ],
   imports: [
     BrowserModule
