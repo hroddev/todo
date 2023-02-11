ca54f93a261241ab5d4d6cddc73320ad35cbb126
Hector Rodriguez <hector@hrod.dev>
2023-02-10 20:06:32 -0500

    add modal and create actions button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild  } from '@angular/core';
+import { Task } from './tasklist/task';
+import { TaskService } from './tasklist/tasks.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,93 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'todo';
+
+  tasks: Task[] = [];
+  editTask: Task | undefined;
+  taskTitle = '';
+
+  constructor(
+    private taskService: TaskService
+  ) { }
+
+  @ViewChild('taskEditInput')
+  set taskEditInput(element: ElementRef<HTMLInputElement>) {
+    if (element) {
+      element.nativeElement.focus();
+    }
+  }
+
+  ngOnInit() {
+    this.getTasks();
+  }
+
+  getTasks(): void {
+    this.taskService.getTasks().subscribe(tasks => (this.tasks = tasks));
+  }
+
+  add(title: string): void {
+    this.editTask = undefined;
+    title = title.trim();
+    if (!title){
+      return;
+    }
+
+    // the server will generate the id or this new task
+    const newTask: Task = { title } as Task;
+    this.taskService
+      .addTask(newTask)
+      .subscribe(task => this.tasks.push(task));
+  }
+
+  edit(taskTitle: string){
+    this.update(taskTitle);
+  }
+
+  search(searchTerm: string){
+    this.editTask = undefined;
+    if(searchTerm){
+      this.taskService
+      .searchTasks(searchTerm)
+      .subscribe(tasks => (this.tasks = tasks));
+    } else {
+      this.getTasks();
+    }
+  }
+
+  update(taskTitle: string){
+    if (taskTitle && this.editTask && this.editTask.title !== taskTitle){
+      this.taskService
+      .updateTask({ ...this.editTask, title: taskTitle})
+      .subscribe( task => {
+        const e = task ? this.tasks.findIndex(t => t.id === task.id) : -1;
+        if (e > -1) {
+          this.tasks[e] = task;
+        }
+      });
+      this.editTask = undefined;
+    }
+  }
+
+  public onOpenModal(task: Task, mode: string):void {
+    const button = document.createElement('button');
+    button.type = 'button';
+    button.style.display = 'none';
+    button.setAttribute('data-toggle', 'modal');
+
+    if (mode === 'add') {
+      button.setAttribute('data-target', 'addtaskmodal');
+    }
+
+    if (mode === 'edittaskmodal') {
+      button.setAttribute('data-target', 'addtaskmodal');
+    }
+
+    if (mode === 'deletetaskmodal') {
+      button.setAttribute('data-target', 'addtaskmodal');
+    }
+  }
+
+//* https://getbootstrap.com/docs/3.4/javascript/#modals*/
+
+
 }
--- src/app/tasklist/tasklist.component.ts
+++ src/app/tasklist/tasklist.component.ts
@@ -10,73 +10,7 @@ import { TaskService } from './tasks.service';
   providers: [TaskService]
 })
 
-export class TasklistComponent implements OnInit {
+export class TasklistComponent {
 
-  tasks: Task[] = [];
-  editTask: Task | undefined;
-  taskTitle = '';
-
-  constructor(
-    private taskService: TaskService
-  ) { }
-
-  @ViewChild('taskEditInput')
-  set taskEditInput(element: ElementRef<HTMLInputElement>) {
-    if (element) {
-      element.nativeElement.focus();
-    }
-  }
-
-  ngOnInit() {
-    this.getTasks();
-  }
-
-  getTasks(): void {
-    this.taskService.getTasks().subscribe(tasks => (this.tasks = tasks));
-  }
-
-  add(title: string): void {
-    this.editTask = undefined;
-    title = title.trim();
-    if (!title){
-      return;
-    }
-
-    // the server will generate the id or this new task
-    const newTask: Task = { title } as Task;
-    this.taskService
-      .addTask(newTask)
-      .subscribe(task => this.tasks.push(task));
-  }
-
-  edit(taskTitle: string){
-    this.update(taskTitle);
-  }
-
-  search(searchTerm: string){
-    this.editTask = undefined;
-    if(searchTerm){
-      this.taskService
-      .searchTasks(searchTerm)
-      .subscribe(tasks => (this.tasks = tasks));
-    } else {
-      this.getTasks();
-    }
-  }
-
-  update(taskTitle: string){
-    if (taskTitle && this.editTask && this.editTask.title !== taskTitle){
-      this.taskService
-      .updateTask({ ...this.editTask, title: taskTitle})
-      .subscribe( task => {
-        const e = task ? this.tasks.findIndex(t => t.id === task.id) : -1;
-        if (e > -1) {
-          this.tasks[e] = task;
-        }
-      });
-      this.editTask = undefined;
-    }
-  }
-
-
+ 
 }
